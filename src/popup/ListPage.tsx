088ea78a629e3da6bea9e37587f2598e021ebf97
mirumi <mail@mirumi.tech>
2025-05-12 22:00:42 +0900

    fix: case of no registered icons on list

--- src/popup/ListPage.tsx
+++ src/popup/ListPage.tsx
@@ -74,38 +74,42 @@ export const ListPage = ({ navigateRegisterPage }: Props) => {
         className=" overflow-y-auto"
         style={{ height: `${444 - 62 /* header */ - 12 /* padding-top */}px` }}
       >
-        {favicons.map((favicon) => (
-          <div
-            className="flex justify-between items-center gap-[13px] h-[37px] pb-[1px] px-[10px] rounded-[7px] hover:bg-[#454B55]"
-            key={favicon.url}
-          >
-            <div className="flex-shrink-0 w-[24px]">
-              <img src={favicon.changeTo} alt={`favicon-${favicon.url}`} />
-            </div>
-            <div className="flex-grow overflow-hidden">
-              <button
-                type="button"
-                className="inline-block w-full text-[14px] text-left text-ellipsis overflow-hidden whitespace-nowrap hover:underline"
-                title={favicon.url}
-                onClick={() => navigateRegisterPage(favicon.url)}
-              >
-                {favicon.url}
+        {favicons.length === 0 ? (
+          <div className="text-[14px] text-center">No registered favicons</div>
+        ) : (
+          favicons.map((favicon) => (
+            <div
+              className="flex justify-between items-center gap-[13px] h-[37px] pb-[1px] px-[10px] rounded-[7px] hover:bg-[#454B55]"
+              key={favicon.url}
+            >
+              <div className="flex-shrink-0 w-[24px]">
+                <img src={favicon.changeTo} alt={`favicon-${favicon.url}`} />
+              </div>
+              <div className="flex-grow overflow-hidden">
+                <button
+                  type="button"
+                  className="inline-block w-full text-[14px] text-left text-ellipsis overflow-hidden whitespace-nowrap hover:underline"
+                  title={favicon.url}
+                  onClick={() => navigateRegisterPage(favicon.url)}
+                >
+                  {favicon.url}
+                </button>
+              </div>
+              <div className="flex-shrink-0 flex items-center">
+                <Switch
+                  checked={favicon.enabled}
+                  className="group inline-flex w-[27px] h-[12px] items-center rounded-full bg-[#7B7B7B] translate-y-[0.5px] transition data-checked:bg-primary"
+                  onChange={() => toggleEnabled(favicon.url)}
+                >
+                  <span className="w-[15px] h-[15px] rounded-full bg-[#C4C4C4] translate-y-[-0.5px] transition group-data-checked:bg-[#F2F2F2] group-data-checked:translate-x-[15px]" />
+                </Switch>
+              </div>
+              <button type="button" className="flex-shrink-0" onClick={() => delete_(favicon.url)}>
+                <FontAwesomeIcon icon={faTrash} className="pt-[2px] text-[16px] text-[#74767D]" />
               </button>
             </div>
-            <div className="flex-shrink-0 flex items-center">
-              <Switch
-                checked={favicon.enabled}
-                className="group inline-flex w-[27px] h-[12px] items-center rounded-full bg-[#7B7B7B] translate-y-[0.5px] transition data-checked:bg-primary"
-                onChange={() => toggleEnabled(favicon.url)}
-              >
-                <span className="w-[15px] h-[15px] rounded-full bg-[#C4C4C4] translate-y-[-0.5px] transition group-data-checked:bg-[#F2F2F2] group-data-checked:translate-x-[15px]" />
-              </Switch>
-            </div>
-            <button type="button" className="flex-shrink-0" onClick={() => delete_(favicon.url)}>
-              <FontAwesomeIcon icon={faTrash} className="pt-[2px] text-[16px] text-[#74767D]" />
-            </button>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   )
