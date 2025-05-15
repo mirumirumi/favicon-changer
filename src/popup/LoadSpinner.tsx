5f6e376a2f5d69277a9021cf508c7989de7d2013
mirumi <mail@mirumi.tech>
2025-05-15 15:43:48 +0900

    fix: no svg title tag

--- src/popup/LoadSpinner.tsx
+++ src/popup/LoadSpinner.tsx
@@ -12,7 +12,6 @@ export const LoadSpinner = ({ size = "100%", color = "#8b8b8b" }: Props) => {
       stroke={color}
       className="absolute inset-0 m-auto animate-spin"
     >
-      <title>loading</title>
       <g fill="none" fillRule="evenodd" transform="translate(1 1)" strokeWidth="4.5">
         <circle strokeOpacity=".5" cx="18" cy="18" r="18" stroke={color} />
         <path d="M36 18c0-9.94-8.06-18-18-18" />
